a366b259d2fe77529b9436bc924b66d2622e709f
Tinarst <teanarostamkhani@gmail.com>
2025-10-29 21:54:15 +0330

    Full Project

--- requirment/logs.py
+++ requirment/logs.py
@@ -3,7 +3,7 @@ import logging
 logging.basicConfig(
     level = logging.INFO,
     format = "%(asctime)s-%(levelname)s-%(message)s",
-    filename = "logger.log",
+    filename = "logs.log",
     filemode = "a"
 )
 
